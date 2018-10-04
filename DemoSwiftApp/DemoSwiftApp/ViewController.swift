7911081f3eb76fb18bf1202890b53e5fdb7ac981
baielvegas <30592102+baielvegas@users.noreply.github.com>
2018-10-04 16:23:01 +0600

    Added labels in Table

--- DemoSwiftApp/DemoSwiftApp/ViewController.swift
+++ DemoSwiftApp/DemoSwiftApp/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let weathertext = ["Monday, 01 October, Sunny, 20C", "Tuesday, 02 October, Rainy, 13C"]
+    let textCellIdentifier = "weatherCell"
+  
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+        
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weathertext.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
---
+++ DemoSwiftApp/DemoSwiftApp/WeatherTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherTableViewCell.swift
+//  DemoSwiftApp
+//
+//  Created by Isaev Baiel on 04.10.2018.
+//  Copyright © 2018 Neobis. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+     
+    }
+
+}
